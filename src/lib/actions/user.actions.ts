e115025e97a52c30c2e5bc7e7266a4514f3f41ed
Vaibhav <vaibhavchaubey786@gmail.com>
2025-01-10 03:01:46 +0530

    add verifySecret function for OTP verification

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { appwriteConfig } from '../appwrite/config';
 import { ID, Query } from 'node-appwrite';
 import { avatarPlaceholderUrl } from '@/constants';
 import { parseStringify } from '../utils';
+import { cookies } from 'next/headers';
 
 /* **Create account flow**
 
@@ -33,7 +34,7 @@ const handleError = (error: unknown, message: string) => {
   throw error;
 };
 
-const sendEmailOTP = async ({ email }: { email: string }) => {
+export const sendEmailOTP = async ({ email }: { email: string }) => {
   const { account } = await createAdminClient();
 
   try {
@@ -77,4 +78,29 @@ export const createAccount = async ({
   }
 
   return parseStringify({ accountId });
+};
+
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
+  try {
+    const { account } = await createAdminClient();
+
+    const session = await account.createSession(accountId, password);
+
+    (await cookies()).set('appwrite-session', session.secret, {
+      path: '/',
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError(error, 'Failed to verify OTP');
+  }
 };
