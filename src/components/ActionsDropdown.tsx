016988da4ca8d099480efec5707780ea583e05b9
Vaibhav <vaibhavchaubey786@gmail.com>
2025-01-13 01:45:43 +0530

    feat: add Page component to display user files; implement Card, Sort, and ActionsDropdown components for file representation and sorting

---
+++ src/components/ActionsDropdown.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ActionsDropdown = () => {
+  return <div>ActionsDropdown</div>;
+};
+
+export default ActionsDropdown;
--- src/lib/actions/file.actions.ts
+++ src/lib/actions/file.actions.ts
@@ -94,6 +94,8 @@ export const getFiles = async () => {
       queries
     );
 
+    console.log('FILES', files);
+
     return parseStringify(files);
   } catch (error) {
     handleError(error, 'Failed to get files');
