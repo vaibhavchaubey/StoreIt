bba415c94e864f9432130f01285895301ec3a9b4
Vaibhav <vaibhavchaubey786@gmail.com>
2025-01-12 19:00:37 +0530

    refactor: update FileUploader component to accept ownerId and accountId props; add signInUser function for OTP handling; integrate Toaster component for notifications

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -10,7 +10,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { createAccount } from '@/lib/actions/user.actions';
+import { createAccount, signInUser } from '@/lib/actions/user.actions';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -52,10 +52,13 @@ const AuthForm = ({ type }: { type: FormType }) => {
     setErrorMessage('');
 
     try {
-      const user = await createAccount({
-        fullName: values.fullName || '',
-        email: values.email,
-      });
+      const user =
+        type === 'sign-up'
+          ? await createAccount({
+              fullName: values.fullName || '',
+              email: values.email,
+            })
+          : await signInUser({ email: values.email });
 
       setAccountId(user.accountId);
     } catch {
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -142,3 +142,19 @@ export const signOutUser = async () => {
     redirect('/sign-in');
   }
 };
+
+export const signInUser = async ({ email }: { email: string }) => {
+  try {
+    const existingUser = await getUserByEmail(email);
+
+    // User exists, send OTP
+    if (existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({ accountId: existingUser.accountId });
+    }
+
+    return parseStringify({ accountId: null, error: 'User not found' });
+  } catch (error) {
+    handleError(error, 'Failed to sign in user');
+  }
+};
