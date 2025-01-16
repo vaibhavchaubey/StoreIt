d600d29a60e56f3491e2752cda3ebc34de70db65
Vaibhav <vaibhavchaubey786@gmail.com>
2025-01-17 02:11:24 +0530

    feat: enhance file retrieval with search and sorting; add debounce to search input for improved performance

--- src/app/(root)/[type]/page.tsx
+++ src/app/(root)/[type]/page.tsx
@@ -1,13 +1,18 @@
 import Card from '@/components/Card';
 import Sort from '@/components/Sort';
 import { getFiles } from '@/lib/actions/file.actions';
+import { getFileTypesParams } from '@/lib/utils';
 import { Models } from 'node-appwrite';
 import React from 'react';
 
-const Page = async ({ params }: SearchParamProps) => {
-  const type = ((await params).type as string) || 'default';
+const Page = async ({ searchParams, params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || '';
+  const searchText = ((await searchParams)?.query as string) || '';
+  const sort = ((await searchParams)?.sort as string) || '';
 
-  const files = await getFiles();
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types, searchText, sort });
 
   return (
     <div className="page-container">
--- src/lib/actions/file.actions.ts
+++ src/lib/actions/file.actions.ts
@@ -63,7 +63,13 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number
+) => {
   const queries = [
     Query.or([
       Query.equal('owner', [currentUser.$id]),
@@ -71,12 +77,34 @@ const createQueries = (currentUser: Models.Document) => {
     ]),
   ];
 
-  // TODO: Search, sort, limits...
+  if (types.length > 0) {
+    queries.push(Query.equal('type', types));
+  }
+
+  if (searchText) {
+    queries.push(Query.contains('name', searchText));
+  }
+
+  if (limit) {
+    queries.push(Query.limit(limit));
+  }
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split('-');
+    queries.push(
+      orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+    );
+  }
 
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({
+  types = [],
+  searchText = '',
+  sort = '$createdAt-desc',
+  limit,
+}: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
@@ -86,7 +114,7 @@ export const getFiles = async () => {
       throw new Error('User not found');
     }
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
 
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
